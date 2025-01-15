4d37ce906c02f81307d3897f862c3fc12da0a6a4
Emerson <emerson.day@gmail.com>
2025-01-14 19:25:51 -0800

    added in-memory data store

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"html/template"
+	"sync"
+)
+
+// Data store (in-memory)
+var (
+	mu       sync.Mutex
+	messages []string
+)
+
+// Template rendering
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
 func main() {
 
 }
