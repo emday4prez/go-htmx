11af4d0d68e3fee23ac3a53b54a8804cef912b2b
Emerson <emerson.day@gmail.com>
2025-01-21 20:10:02 -0800

    add message handler

--- main.go
+++ main.go
@@ -17,11 +17,23 @@ var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 func main() {
 	http.HandleFunc("/", indexHandler)
-
+	http.HandleFunc("/add", addHandler)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	defer mu.Unlock()
 	tmpl.Execute(w, messages)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		msg := r.FormValue("message")
+		mu.Lock()
+		messages = append(messages, msg)
+		mu.Unlock()
+
+		// Respond with the new item for HTMX to swap in
+		w.Write([]byte("<li>" + msg + ` <button hx-post="/delete" hx-vals='{"message":"` + msg + `"}'>Delete</button></li>`))
+	}
 }
